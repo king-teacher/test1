2fbeedb21dbf9a3e0d3f549413dc1900492ca9a1
吕布 <lb@com>
2021-02-18 10:37:57 +0800

    创建Demo3

---
+++ src/main/java/cn/rl/demo/Demo3.java
@@ -0,0 +1,15 @@
+/**
+ * FileName:Demo3
+ * Author:Administrator
+ * Date:2021/2/18 10:37
+ * <autor>  <time>  <version>  <desc>
+ * 作者姓名  修改时间   版本号     描述
+ */
+package cn.rl.demo;
+
+public class Demo3 {
+
+	public void printStr(String str) {
+		System.out.println(str);
+	}
+}
