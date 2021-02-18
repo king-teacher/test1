3732c767628ea93d800991e73819273aa9b3c31d
吕布 <lb@com>
2021-02-18 10:36:09 +0800

    创建Demo2

---
+++ src/main/java/cn/rl/demo/Demo2.java
@@ -0,0 +1,14 @@
+/**
+ * FileName:Demo2
+ * Author:Administrator
+ * Date:2021/2/18 10:34
+ * <autor>  <time>  <version>  <desc>
+ * 作者姓名  修改时间   版本号     描述
+ */
+package cn.rl.demo;
+
+public class Demo2 {
+	public void printStr() {
+		System.out.println("hello world");
+	}
+}
